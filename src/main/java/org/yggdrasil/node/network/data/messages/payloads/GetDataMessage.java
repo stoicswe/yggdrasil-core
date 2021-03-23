dae46f327c31d1a161ec91212b7cb869a9f72646
Nathaniel Bunch <31823043+nathanielbunch@users.noreply.github.com>
2021-03-23 19:09:42 -0400

    Messages impl (#26)
    * added messages defintions

--- src/main/java/org/yggdrasil/node/network/data/messages/payloads/GetDataMessage.java
+++ src/main/java/org/yggdrasil/node/network/data/messages/payloads/GetDataMessage.java
@@ -2,5 +2,85 @@ package org.yggdrasil.node.network.data.messages.payloads;
 
 import org.yggdrasil.node.network.data.messages.MessagePayload;
 
+/**
+ * The Get Data message is used by a node to retrieve data from another node.
+ * That data could be block related (blocks, chain, or transactions) and in that case
+ * the object hashes will be populated. Otherwise, the message is a command message,
+ * used by the distributed system for system data, such as IP addresses.
+ *
+ * @since 0.0.1
+ * @author nathanielbunch
+ */
 public class GetDataMessage implements MessagePayload {
+
+    private final int version;
+    private final char[] type;
+    private final int hashCount;
+    private final char[][] objectHashes;
+    private final char[] stopHash;
+
+    private GetDataMessage(Builder builder) {
+        this.version = builder.version;
+        this.type = builder.type;
+        this.hashCount = builder.hashCount;
+        this.objectHashes = builder.objectHashes;
+        this.stopHash = builder.stopHash;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public char[] getType() {
+        return type;
+    }
+
+    public int getHashCount() {
+        return hashCount;
+    }
+
+    public char[][] getObjectHashes() {
+        return objectHashes;
+    }
+
+    public char[] getStopHash() {
+        return stopHash;
+    }
+
+    public static class Builder {
+
+        private int version;
+        private char[] type;
+        private int hashCount;
+        private char[][] objectHashes;
+        private char[] stopHash;
+
+        private Builder(){}
+
+        public Builder setVersion(int version) {
+            this.version = version;
+            return this;
+        }
+
+        public Builder setType(char[] type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setHashCount(int hashCount) {
+            this.hashCount = hashCount;
+            return this;
+        }
+
+        public Builder setObjectHashes(char[][] objectHashes) {
+            this.objectHashes = objectHashes;
+            return this;
+        }
+
+        public Builder setStopHash(char[] stopHash) {
+            this.stopHash = stopHash;
+            return this;
+        }
+
+    }
 }
--- src/main/java/org/yggdrasil/node/network/data/messages/payloads/HeaderMessage.java
+++ src/main/java/org/yggdrasil/node/network/data/messages/payloads/HeaderMessage.java
@@ -1,6 +1,70 @@
 package org.yggdrasil.node.network.data.messages.payloads;
 
 import org.yggdrasil.node.network.data.messages.MessagePayload;
+import org.yggdrasil.node.network.data.messages.enums.HeaderType;
 
+/**
+ * The Header message is a response to the getData message that is requesting
+ * either block or transaction headers. The internal headerPayload property
+ * contains the actual headers (block or transaction).
+ *
+ * @since 0.0.1
+ * @author nathanielbunch
+ */
 public class HeaderMessage implements MessagePayload {
+
+    private final int headerCount;
+    private final char[] headerType;
+    private final HeaderPayload[] headers;
+
+    private HeaderMessage(Builder builder) {
+        this.headerCount = builder.headerCount;
+        this.headerType = builder.headerType;
+        this.headers = builder.headers;
+    }
+
+    public int getHeaderCount() {
+        return headerCount;
+    }
+
+    public char[] getHeaderType() {
+        return headerType;
+    }
+
+    public HeaderPayload[] getHeaders() {
+        return headers;
+    }
+
+    public static class Builder {
+
+        private int headerCount;
+        private char[] headerType;
+        private HeaderPayload[] headers;
+
+        private Builder(){}
+
+        public static Builder newBuilder() {
+            return new Builder();
+        }
+
+        public Builder setHeaderCount(int headerCount) {
+            this.headerCount = headerCount;
+            return this;
+        }
+
+        public Builder setHeaderType(HeaderType headerType) {
+            this.headerType = headerType.getValue();
+            return this;
+        }
+
+        public Builder setHeaders(HeaderPayload[] headers) {
+            this.headers = headers;
+            return this;
+        }
+
+        public HeaderMessage build() {
+            return new HeaderMessage(this);
+        }
+
+    }
 }
