90f1a0cd213877c2aebf0092df54c0756217361d
Nathaniel Bunch <31823043+nathanielbunch@users.noreply.github.com>
2021-04-04 16:22:01 -0400

    added correct network enum use...take 2

--- src/main/java/org/yggdrasil/node/network/runners/HandshakeRunner.java
+++ src/main/java/org/yggdrasil/node/network/runners/HandshakeRunner.java
@@ -68,7 +68,7 @@ public class HandshakeRunner implements Runnable {
                         .build();
                 // build the message
                 sentMessage = Message.Builder.newBuilder()
-                        .setNetwork(NetworkType.getByValue(nodeConfig.getNetwork().toCharArray()))
+                        .setNetwork(NetworkType.getByValue(nodeConfig.getNetwork().toUpperCase(Locale.ROOT).toCharArray()))
                         .setRequestType(RequestType.HANDSHAKE_OFFR)
                         .setPayloadSize(BigInteger.valueOf(GraphLayout.parseInstance(offerHandshake).totalSize()))
                         .setMessagePayload(offerHandshake)
