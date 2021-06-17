6aa2de79f1e636a3936a4db3398d0279a482402e
Nathaniel Bunch <31823043+nathanielbunch@users.noreply.github.com>
2021-06-17 18:05:37 -0400

    added some addressMessage impl changes

--- src/main/java/org/yggdrasil/node/network/messages/handlers/AddressResponseMessageHandler.java
+++ src/main/java/org/yggdrasil/node/network/messages/handlers/AddressResponseMessageHandler.java
@@ -4,17 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.yggdrasil.node.network.messages.MessagePayload;
 import org.yggdrasil.node.network.messages.MessagePool;
+import org.yggdrasil.node.network.messages.payloads.AddressMessage;
 import org.yggdrasil.node.network.messages.payloads.AddressPayload;
 import org.yggdrasil.node.network.runners.NodeConnection;
 
 @Component
-public class AddressResponseMessageHandler implements MessageHandler<AddressPayload> {
+public class AddressResponseMessageHandler implements MessageHandler<AddressMessage> {
 
     @Autowired
     private MessagePool messagePool;
 
     @Override
-    public MessagePayload handleMessagePayload(AddressPayload addressPayload, NodeConnection nodeConnection) {
+    public MessagePayload handleMessagePayload(AddressMessage addressMessage, NodeConnection nodeConnection) {
+        // if the count of IPs does not match the length of addressMessages in the message
+        // then send an acknowledgement and ignore the payload data
+        
         return null;
     }
 
