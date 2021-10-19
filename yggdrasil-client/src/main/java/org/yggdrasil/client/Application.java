fddd9474407bcc8f05d5d3fb49b29269cb31e731
Nathaniel Bunch <31823043+stoicswe@users.noreply.github.com>
2021-10-19 08:49:42 -0400

    added start to the client project

---
+++ yggdrasil-client/src/main/java/org/yggdrasil/client/Application.java
@@ -0,0 +1,10 @@
+package org.yggdrasil.client;
+
+/**
+ * The startpoint of the GUI application.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
