adceaaa8caae28184b2b61672286615012316743
Nathaniel Bunch <stoicswe@protonmail.com>
2021-11-14 21:55:30 -0500

    added settings UI section

--- yggdrasil-node/src/main/java/org/yggdrasil/ui/components/menubar/SettingsMenuSection.java
+++ yggdrasil-node/src/main/java/org/yggdrasil/ui/components/menubar/SettingsMenuSection.java
@@ -1,16 +1,26 @@
 package org.yggdrasil.ui.components.menubar;
 
+import org.yggdrasil.ui.components.menubar.settings.ChangePassphraseMenuItem;
+import org.yggdrasil.ui.components.menubar.settings.EncryptWalletMenuItem;
+import org.yggdrasil.ui.components.menubar.settings.MaskValuesMenuItem;
+import org.yggdrasil.ui.components.menubar.settings.OptionsMenuItem;
+
 import javax.swing.*;
 
 public class SettingsMenuSection extends JMenu {
 
+    JMenuItem encryptWallet = new EncryptWalletMenuItem("Encrypt Wallet");
+    JMenuItem changePassphrase = new ChangePassphraseMenuItem("Change Passphrase");
+    JMenuItem maskValues = new MaskValuesMenuItem("Mask Values");
+    JMenuItem options = new OptionsMenuItem("Options...");
+
     public SettingsMenuSection(String label) {
-        this.add(new JMenuItem("Encrypt Wallet"));
-        this.add(new JMenuItem("Change Passphrase"));
+        this.add(this.encryptWallet);
+        this.add(this.changePassphrase);
         this.addSeparator();
-        this.add(new JMenuItem("Mask Values"));
+        this.add(this.maskValues);
         this.addSeparator();
-        this.add(new JMenuItem("Options..."));
+        this.add(this.options);
     }
 
 }
---
+++ yggdrasil-node/src/main/java/org/yggdrasil/ui/components/menubar/settings/MaskValuesMenuItem.java
@@ -0,0 +1,11 @@
+package org.yggdrasil.ui.components.menubar.settings;
+
+import javax.swing.*;
+
+public class MaskValuesMenuItem extends JMenuItem {
+
+    public MaskValuesMenuItem(String label) {
+        this.setText(label);
+    }
+
+}
